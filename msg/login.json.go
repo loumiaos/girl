a5998d89616fa9a0536b768068c80a62356eb611
snowyyj001 <916040065@qq.com>
2020-02-10 18:08:49 +0800

    json消息协议添加

---
+++ msg/login.json.go
@@ -0,0 +1,33 @@
+package msg
+
+type C_A_Login struct {
+	AccountName string `json:"account_name"`;
+	Password string;
+	Channel int;
+	LoginType int;
+	HeadIcon string;
+	Sex int;
+	NickName string;
+}
+
+type A_C_Login struct{
+	ErrorStr string;
+	UserID  int;
+}
+
+type C_S_Login struct{
+	UserID int;
+}
+
+message S_C_Login {
+	ErrorStr string;
+	UserID int;
+	Gold int64;    
+	Coin int64;
+	Money int64;
+	HeadIconUrl string;
+	UnderWrite string;
+	Sex int;
+	ActiveFlag int64;
+	NickName string;
+}
